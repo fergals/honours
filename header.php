9e6cc45ba853038bea10b9533c417e5fe081bc89
Fergal <ferghal@gmail.com>
2016-07-11 22:54:26 +0100

    Display user info and added CSS

--- header.php
+++ header.php
@@ -1,10 +1,23 @@
-<html>
-<head>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
   <title>Help! - Online Ticketing System</title>
-  <link rel="stylesheet" type="text/css" href="style.css">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="css/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
 <body>
-<div id="navigation">
-  <a href="index.php">Home</a> // <a href="submit.php">Submit</a> // View // Logout
-</div>
+    <nav class="navbar navbar-static-top navbar-dark bg-inverse">
+      <a class="navbar-brand" href="#">HELP</a>
+      <ul class="nav navbar-nav">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="submit.php">Submit New Ticket</a>
+        </li>
+      </ul>
+    </nav>
+    <div class="container">
--- ticket.php
+++ ticket.php
@@ -6,14 +6,15 @@ include 'header.php'; ?>
   <h1>Your selected ticket</h1>
 
 <?php
-$id = $_GET['id'];
-$sql = "SELECT * FROM ticket WHERE id = '$id'";
+$ticketid = $_GET['id'];
+$sql = "SELECT * FROM ticket WHERE id = '$ticketid'";
+$username = "Select username FROM users WHERE id = 1";
 $result = $con->query($sql);
 
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "<div='ticket'>" . $row["tID"] . " Submitted by: [user] " . "(" . $row["status"] . ")<br />";
+        echo "<div='ticket'>" . $row["tID"] . " Submitted by: ". $username . "(" . $row["status"] . ")<br />";
         echo $row["query"];
         echo "<div id='ticket-reply'>
               <form action='submitsqli.php' method='post' /><p>
