8f473174df2c3b32f2912526e00f1219107c1c51
Fergal <ferghal@gmail.com>
2016-07-29 18:32:36 +0100

    Merged with master
    Update files to PDO

--- submit.php
+++ submit.php
@@ -1,17 +1,77 @@
 <?php
 include 'header.php'; ?>
-<div id="tickets">
+  <form class="form-horizontal" action="submitticket.php" method="post" />
+    <div class="form-group">
+      <label for="inputEmail3" class="col-sm-2 control-label">Query</label>
+      <div class="col-sm-10">
+        <textarea class='form-control' rows='5' name='query'></textarea>
+      </div>
+    </div>
 
-  <form action="submitsqli.php" method="post" />
-  <p>
-    Query: <input type="text" name="query" /><br />
-    Status: <select name="status">
-              <option value="open">Open</option>
-              <option value="closed">Closed</option>
-    </select>
-  </p>
-  <input type="submit" value="Submit" / />
-</form>
+    <div class="form-group">
+      <label for="status" class="col-sm-2 control-label">Staus</label>
+      <div class="col-sm-10">
+        <select name="status" class="form-control">
+          <option value="Open">Open</option>
+          <option value="Closed">Closed</option>
+        </select>
+      </div>
+    </div>
+
+    <div class="form-group">
+      <label for="queue" class="col-sm-2 control-label">Queue</label>
+      <div class="col-sm-10">
+        <select name="queue" class="form-control">
+          <option value="1stline">1st Line</option>
+          <option value="2ndline">2nd Line</option>
+          <option value="3rdline">3rd Line</option>
+        </select>
+      </div>
+    </div>
+
+    <div class="form-group">
+      <label for="urgency" class="col-sm-2 control-label">Urgency</label>
+      <div class="col-sm-10">
+        <select name="urgency" class="form-control">
+          <option value="low">Low</option>
+          <option value="normal">Normal</option>
+          <option value="critical">Crtical</option>
+        </select>
+      </div>
+    </div>
+
+    <div class="form-group">
+      <label for="departments" class="col-sm-2 control-label">Departments</label>
+      <div class="col-sm-10">
+        <select name="department" class="form-control">
+          <option value="Accounts">Accounts</option>
+          <option value="Admissions">Admissions</option>
+          <option value="Computing">Computing</option>
+          <option value="Finance">Finance</option>
+          <option value="Marketing">Marketing</option>
+          <option value="Student">Student Issues</option>
+        </select>
+      </div>
+    </div>
+
+    <div class="form-group">
+      <label for="category" class="col-sm-2 control-label">Category</label>
+      <div class="col-sm-10">
+        <select name="category" class="form-control">
+          <option value="Computing">Computing</option>
+          <option value="Money Issues">Money Issues</option>
+        </select>
+      </div>
+    </div>
+
+
+    <div class="form-group">
+      <div class="col-sm-offset-2 col-sm-10">
+        <button type="submit" class="btn btn-default">Submit</button>
+      </div>
+    </div>
+  </form>
+
 
 </div>
 <?php
--- ticket.php
+++ ticket.php
@@ -4,48 +4,54 @@ include 'header.php'; ?>
 
 <div class="container">
 <div class="row">
-  <div class="col-xs-4">
-    <strong>I2016-07-07003</strong><br />
-    Queue: 1st Line<br />
-    Status: Open<br />
-    Urgency: Normal<br /></br>
+<div class="col-xs-4">
 
-    Submitted: <br /><br />
+<?php
+ $ticketid1 = $_GET['id'];
+ $ticketinfo = $db->query("SELECT userid, tID, date, query FROM ticket where id = '$ticketid1'");
+ while($r = $ticketinfo->fetch(PDO::FETCH_OBJ)) {
+echo '<strong>' . $r->tID . '</strong><br />';
+echo 'Queue: First Line<br />';
+echo 'Status: Open<br />';
+echo 'Urgency: Normal<br /><br />';
+echo 'Category: Computer Issues<br />';
+echo 'Department: Finance <br /><br />';
+echo 'Submitted: 10/10/16<br /><br />';
+}
+$userinfo = $db->query("SELECT id, username, firstname, surname, email, phonenumber, department, usertype FROM users where id = 1");
+while($u = $userinfo->fetch(PDO::FETCH_OBJ)) {
+echo '<strong>User Information</strong><br />';
+echo 'Name: ' . $u->firstname . ' ' . $u->surname . '<br />';
+echo 'E-mail: ' . $u->email . '<br />';
+echo 'Telephone: ' . $u->phonenumber .'<br />';
+echo 'Department: ' . $u->department . '<br />';
+echo 'User Type: ' . $u->usertype;
+}
+ ?>
 
-    <strong>User Information</strong><br />
-    Name:<br />
-    E-mail:<br />
-    Department:<br />
-
-  </div>
-
+</div>
 
   <div class="col-xs-6">
-  <?php
-  $ticketid = $_GET['id'];
-  $sql = "SELECT * FROM ticket WHERE id = '$ticketid'";
-  $result = $con->query($sql);
+<?php
+    $ticketid = $_GET['id'];
+    $result = $db->query("SELECT userid, tID, date, query FROM ticket where id = '$ticketid'");
+    while($r = $result->fetch(PDO::FETCH_OBJ)) {
+      echo $r->query;}
+?>
 
-  if ($result->num_rows > 0) {
-      // output data of each row
-      while($row = $result->fetch_assoc()) {
-          echo $row["query"] . "<br /><br />";
-          echo "<hr />";
+<br />
+<br />
 
-          echo "<div class='form-group'>
-                <form action='submitsqli.php' method='post'>
-                <textarea class='form-control' rows='5' id='comment'></textarea>
-                <button type='submit' class='btn btn-default'>Submit</button>
-                </form>
-                </div>";
-      }
+<hr />
+    <div class='form-group'>
+      <form action='submitcomment.php' method='post'>
+        <textarea class='form-control' rows='5' name='comment'></textarea>
+        <button type='submit' class='btn btn-default'>Submit</button>
+      </form>
+    </div>
 
-  } else {
-      echo "No Tickets Found";
-  }
 
-  ?>
-  </div>
+</div>
 </div>
 
 </div>
